e27bd49b10b9ac290197d360f9a14b72653466b3
Abhishek Wadhawan <abhishek.wadhawan@ymail.com>
2021-02-21 20:03:50 +0530

    Updates

--- index.js
+++ index.js
@@ -1,3 +1,205 @@
+const arrayInput = [
+  1470,
+  1577,
+  1054,
+  1962,
+  1107,
+  1123,
+  1683,
+  1680,
+  1176,
+  1917,
+  1786,
+  1565,
+  1464,
+  1097,
+  1363,
+  1091,
+  1072,
+  1821,
+  1514,
+  1526,
+  1633,
+  1953,
+  1594,
+  1386,
+  1611,
+  1354,
+  490,
+  1302,
+  1785,
+  1504,
+  1100,
+  1631,
+  1570,
+  1906,
+  1678,
+  1742,
+  1505,
+  1894,
+  1682,
+  1854,
+  1663,
+  1284,
+  1346,
+  1390,
+  1285,
+  1993,
+  1301,
+  1288,
+  1882,
+  1434,
+  1435,
+  1370,
+  1603,
+  1974,
+  1364,
+  1667,
+  1561,
+  1205,
+  1487,
+  1676,
+  1654,
+  1352,
+  1584,
+  1445,
+  376,
+  1945,
+  1864,
+  1981,
+  2001,
+  1254,
+  1335,
+  1337,
+  1769,
+  1467,
+  1332,
+  2008,
+  1732,
+  1365,
+  1852,
+  1553,
+  1324,
+  1642,
+  1927,
+  1658,
+  1750,
+  1380,
+  1232,
+  1457,
+  1567,
+  1735,
+  1259,
+  1780,
+  1653,
+  1400,
+  1489,
+  1439,
+  1315,
+  1891,
+  1249,
+  1770,
+  1214,
+  1961,
+  92,
+  1479,
+  1901,
+  1331,
+  1476,
+  1969,
+  1480,
+  289,
+  1855,
+  1325,
+  1518,
+  1819,
+  1089,
+  1849,
+  2006,
+  1996,
+  1833,
+  1965,
+  1547,
+  1397,
+  1153,
+  1622,
+  1216,
+  1609,
+  1323,
+  1148,
+  1262,
+  1347,
+  1118,
+  1808,
+  2010,
+  1256,
+  1529,
+  1169,
+  1094,
+  1738,
+  1544,
+  1517,
+  1644,
+  1221,
+  1699,
+  1655,
+  1474,
+  1977,
+  1717,
+  1143,
+  1721,
+  1417,
+  957,
+  1938,
+  1193,
+  1863,
+  1730,
+  1187,
+  1628,
+  1314,
+  1483,
+  1606,
+  1602,
+  1116,
+  835,
+  480,
+  1955,
+  1900,
+  1968,
+  1903,
+  1436,
+  1771,
+  1774,
+  1832,
+  1586,
+  1844,
+  1198,
+  1687,
+  1283,
+  1740,
+  1273,
+  1973,
+  1665,
+  1251,
+  1889,
+  1521,
+  1648,
+  1718,
+  1263,
+  1635,
+  1122,
+  1243,
+  1085,
+  1991,
+  1271,
+  1230,
+  1245,
+  1318,
+  1373,
+  1911,
+  1282,
+  1306,
+];
 const input = [10, 20, 30, 10, 20, 40, 10];
 
 function sockMerchant1(n, ar) {
@@ -135,7 +337,7 @@ function mergeSortArrays(arr1, arr2) {
  * @param {number} target
  * @return {number[]}
  */
-var twoSum = function (nums, target) {
+var twoSum1 = function (nums, target) {
   let low = 0,
     high = nums.length - 1;
   while (low < high) {
@@ -151,9 +353,9 @@ var twoSum = function (nums, target) {
   return [];
 };
 
-// console.log(twoSum([2,7,11,15], 9));
-// console.log(twoSum([2,3,4], 6));
-// console.log(twoSum([2,3,4,5], 9));
+// console.log(twoSum1([1, 2, 3, 7, 5], 12));
+// console.log(twoSum1([2,3,4], 6));
+// console.log(twoSum1([2,3,4,5], 9));
 //
 //
 // Q4: move zeros
@@ -541,9 +743,14 @@ let range1 = {
       last: this.to,
       next() {
         if (this.current <= this.last) {
-          return { done: false, value: this.current++ };
+          return {
+            done: false,
+            value: this.current++
+          };
         } else {
-          return { done: true };
+          return {
+            done: true
+          };
         }
       },
     };
@@ -564,9 +771,14 @@ let range2 = {
       async next() {
         await new Promise((resolve) => setTimeout(resolve, 1000));
         if (this.current <= this.last) {
-          return { done: false, value: this.current++ };
+          return {
+            done: false,
+            value: this.current++
+          };
         } else {
-          return { done: true };
+          return {
+            done: true
+          };
         }
       },
     };
@@ -703,7 +915,9 @@ function deepCopy(obj) {
 const aObj = {
   a: 1,
   b: 2,
-  c: { d: 3 },
+  c: {
+    d: 3
+  },
 };
 const bObj = Object.assign({}, aObj);
 const cObj = deepCopy(aObj);
@@ -735,6 +949,7 @@ const myThrottle = (fn, delay) => {
   let isThrottled = false,
     savedArgs,
     savedThis;
+
   function wrapper() {
     if (isThrottled) {
       savedArgs = arguments;
@@ -746,7 +961,6 @@ const myThrottle = (fn, delay) => {
 
     setTimeout(() => {
       isThrottled = false;
-      console.log(savedArgs);
       if (savedArgs) {
         wrapper.apply(savedThis, savedArgs);
         savedArgs = savedThis = null;
@@ -765,17 +979,190 @@ const myThrottle = (fn, delay) => {
 // xy("f");
 
 function rob(nums) {
-  if(nums === null || nums.length === 0) return 0
-  if(nums.length === 1) return nums[0];
-  if(nums.length === 2) return Math.max(nums[0], nums[1]);
+  if (nums === null || nums.length === 0) return 0
+  if (nums.length === 1) return nums[0];
+  if (nums.length === 2) return Math.max(nums[0], nums[1]);
   let maxMoneyRobbed = []
   maxMoneyRobbed[0] = nums[0];
   maxMoneyRobbed[1] = Math.max(nums[0], nums[1]);
   for (let i = 2; i < nums.length; i++) {
-    maxMoneyRobbed[i] = Math.max(nums[i] + maxMoneyRobbed[i-2], maxMoneyRobbed[i-1])
+    maxMoneyRobbed[i] = Math.max(nums[i] + maxMoneyRobbed[i - 2], maxMoneyRobbed[i - 1])
   }
   return maxMoneyRobbed[nums.length - 1]
 }
 
-console.log(rob([1, 2, 3, 4]));
-console.log(rob([2, 7, 9, 3, 1]));
+// console.log(rob([1, 2, 3, 4]));
+// console.log(rob([2, 7, 9, 3, 1]));
+
+// O(n*3)
+function findSum(nums, sum) {
+  let result = 0
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = 1; j < nums.length; j++) {
+      for (let k = 2; k < nums.length; k++) {
+        if (nums[i] + nums[j] + nums[k] === sum) result = nums[i] * nums[j] * nums[k];
+      }
+
+    }
+  }
+  return result;
+}
+
+
+
+function subArraySum(arr, n, s) {
+  let start = 0,
+    end = 0
+  for (let i = 0; i < n; i++) {
+
+  }
+}
+
+function curry(fn) {
+  return function curried(...args) {
+    if (args.length >= fn.length) {
+      return fn.apply(this, args)
+    } else {
+      return function (...args2) {
+        return curried.apply(this, args.concat(args2));
+      }
+    }
+  }
+}
+
+function mul(a, b, c) {
+  return a * b * c;
+}
+
+let curriedMul = curry(mul)
+// console.log(curriedMul(3, 4, 5));
+
+
+
+
+// var majorityElement = function (nums) {
+//   let ele = 0;
+//   let obj = {};
+//   const majority = nums.length / 2;
+//   for (let i = 0; i < nums.length; i++) {
+//     const e = nums[i]
+//     if (obj[e]) obj[e] = obj[e] + 1;
+//     else obj[e] = 1;
+//   }
+//   for (let k in obj) {
+//     if (obj[k] > majority) ele = k;
+//   }
+//   return ele;
+// };
+
+const majorityElement = (nums) => {
+  nums.sort();
+  const majority = Math.ceil(nums.length / 2);
+  return nums[majority];
+}
+
+// console.log(majorityElement([3,2,3]))
+
+var isAnagram = function (s, t) {
+  if (t.length !== s.length) return false;
+  return sortedStr(s) === sortedStr(t)
+};
+
+const sortedStr = str => str.split("").sort().join("");
+
+// console.log(isAnagram("anagram", "nagaram"));
+// console.log(isAnagram("rat", "cars"));
+
+var firstUniqChar = function (s) {
+  let charObj = {}
+  for (let charIndex in s) {
+    const char = s[charIndex]
+    if (!charObj[char]) charObj[char] = 1
+    else charObj[char] = charObj[char]++;
+  }
+  let index = -1
+  console.log(charObj);
+  for (let key of charObj) {
+    console.log(charObj[key]);
+    if (charObj[key] === 1) {
+      return ++index
+    }
+  }
+  return index
+};
+
+// console.log(firstUniqChar("leetcode"));
+// console.log(firstUniqChar("loveleetcode"));
+
+function subarraySum(arr, n, s) {
+  let currentSum = arr[0],
+    left = 0;
+  right = 0;
+  for (let i = 1; i < n; i++) {
+    if (currentSum === s) {
+      return [left, right];
+    } else {
+      if (currentSum < s) {
+        currentSum = currentSum + arr[i];
+        right++;
+      } else {
+        currentSum = currentSum - arr[i];
+        left++;
+      }
+    }
+  }
+  return []
+}
+
+// console.log(subarraySum([1,2,3,7,5], 5, 12))
+
+Array.prototype.myReduce = function (fn, initial) {
+  let values = this;
+  values.forEach(item => {
+    initial = initial !== undefined ? fn(initial, item) : item
+  })
+  return initial
+}
+
+// console.log([1, 2, 3, 4, 5].myReduce((a, c) => a + c, 0));
+// console.log([1, 2, 3, 4, 5].reduce((a, c) => a + c, 0));
+
+
+var maxArea = function (heights) {
+  let maxArea = 0, p1 = 0, p2 = heights.length - 1;
+  while (p1 < p2) {
+    const height = Math.min(heights[p1], heights[p2]);
+    const width = p2 - p1;
+    const area = height * width;
+    maxArea = Math.max(maxArea, area);
+    if(heights[p1] <= heights[p2]){
+      p1++
+    }else{
+      p2--;
+    }
+  }
+  return maxArea;
+};
+
+// console.log(maxArea([1, 8, 6, 2, 5, 4, 8, 3, 7]));
+// console.log(maxArea([4,3,2,1,4]));
+// console.log(maxArea([1,2,1]));
+
+
+var twoSum = function (nums, target) {
+  let obj = {} 
+  for (let p1 = 0; p1 < nums.length; p1++) {
+    const currentMapVal = obj[nums[p1]]; 
+    if(currentMapVal >= 0){
+      return [p1, currentMapVal]
+    } else{
+      const numToFind = target - nums[p1];
+      obj[numToFind] = p1;
+    }
+  }
+  return []
+};
+
+// console.log(twoSum([2,7,11,15], 9));
+// console.log(twoSum([3, 2, 4], 6));
+// console.log(twoSum([3, 3], 6));
